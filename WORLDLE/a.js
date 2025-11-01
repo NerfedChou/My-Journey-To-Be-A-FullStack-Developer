5b19340832e204bbef08e48e8a9ae18d0e302265
NerfedChou <janrhian012904@gmail.com>
2025-11-02 01:20:58 +0800

    Good now

--- WORLDLE/a.js
+++ WORLDLE/a.js
@@ -3,7 +3,6 @@ const ROUNDS = 6;
 const letters = document.querySelectorAll(".scoreboard-letter");
 const loadingDiv = document.querySelector(".info-bar");
 
-// I like to do an async init function so I can use "await"
 async function init() {
     // the state for the app
     let currentRow = 0;
@@ -11,7 +10,6 @@ async function init() {
     let done = false;
     let isLoading = true;
 
-    // nab the word of the day
     const res = await fetch("https://words.dev-apis.com/word-of-the-day");
     const { word: wordRes } = await res.json();
     const word = wordRes.toUpperCase();
@@ -19,8 +17,8 @@ async function init() {
     isLoading = false;
     setLoading(isLoading);
 
-    // user adds a letter to the current guess
     function addLetter(letter) {
+        let current;
         if (currentGuess.length < ANSWER_LENGTH) {
             currentGuess += letter;
         } else {
@@ -31,15 +29,11 @@ async function init() {
             letter;
     }
 
-    // user tries to enter a guess
     async function commit() {
         if (currentGuess.length !== ANSWER_LENGTH) {
-            // do nothing
             return;
         }
 
-        // check the API to see if it's a valid word
-        // skip this step if you're not checking for valid words
         isLoading = true;
         setLoading(isLoading);
         const res = await fetch("https://words.dev-apis.com/validate-word", {
@@ -50,7 +44,7 @@ async function init() {
         isLoading = false;
         setLoading(isLoading);
 
-        // not valid, mark the word as invalid and return
+
         if (!validWord) {
             markInvalidWord();
             return;
@@ -60,24 +54,20 @@ async function init() {
         const map = makeMap(wordParts);
         let allRight = true;
 
-        // first pass just finds correct letters so we can mark those as
-        // correct first
+
         for (let i = 0; i < ANSWER_LENGTH; i++) {
             if (guessParts[i] === wordParts[i]) {
-                // mark as correct
+
                 letters[currentRow * ANSWER_LENGTH + i].classList.add("correct");
                 map[guessParts[i]]--;
             }
         }
 
-        // second pass finds close and wrong letters
-        // we use the map to make sure we mark the correct amount of
-        // close letters
+
         for (let i = 0; i < ANSWER_LENGTH; i++) {
             if (guessParts[i] === wordParts[i]) {
-                // do nothing
+
             } else if (map[guessParts[i]] && map[guessParts[i]] > 0) {
-                // mark as close
                 allRight = false;
                 letters[currentRow * ANSWER_LENGTH + i].classList.add("close");
                 map[guessParts[i]]--;
@@ -91,7 +81,6 @@ async function init() {
         currentRow++;
         currentGuess = "";
         if (allRight) {
-            // win
             alert("you win");
             document.querySelector(".brand").classList.add("winner");
             done = true;
@@ -102,20 +91,16 @@ async function init() {
         }
     }
 
-    // user hits backspace, if the length of the string is 0 then do
-    // nothing
     function backspace() {
         currentGuess = currentGuess.substring(0, currentGuess.length - 1);
         letters[currentRow * ANSWER_LENGTH + currentGuess.length].innerText = "";
     }
 
-    // let the user know that their guess wasn't a real word
-    // skip this if you're not doing guess validation
+
     function markInvalidWord() {
         for (let i = 0; i < ANSWER_LENGTH; i++) {
             letters[currentRow * ANSWER_LENGTH + i].classList.remove("invalid");
 
-            // long enough for the browser to repaint without the "invalid class" so we can then add it again
             setTimeout(
                 () => letters[currentRow * ANSWER_LENGTH + i].classList.add("invalid"),
                 10
@@ -123,11 +108,8 @@ async function init() {
         }
     }
 
-    // listening for event keys and routing to the right function
-    // we listen on keydown so we can catch Enter and Backspace
     document.addEventListener("keydown", function handleKeyPress(event) {
         if (done || isLoading) {
-            // do nothing;
             return;
         }
 
@@ -140,27 +122,19 @@ async function init() {
         } else if (isLetter(action)) {
             addLetter(action.toUpperCase());
         } else {
-            // do nothing
+
         }
     });
 }
 
-// a little function to check to see if a character is alphabet letter
-// this uses regex (the /[a-zA-Z]/ part) but don't worry about it
-// you can learn that later and don't need it too frequently
 function isLetter(letter) {
     return /^[a-zA-Z]$/.test(letter);
 }
 
-// show the loading spinner when needed
+
 function setLoading(isLoading) {
     loadingDiv.classList.toggle("hidden", !isLoading);
 }
-
-// takes an array of letters (like ['E', 'L', 'I', 'T', 'E']) and creates
-// an object out of it (like {E: 2, L: 1, T: 1}) so we can use that to
-// make sure we get the correct amount of letters marked close instead
-// of just wrong or correct
 
 function makeMap(array) {
     const obj = {};
@@ -175,3 +149,5 @@ function makeMap(array) {
 }
 
 init();
+
+//oten
